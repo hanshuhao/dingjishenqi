60d11619e004aebffb906ac7e973102ae5b658de
hanshuhao <381611213@qq.com>
2016-10-07 14:26:38 +0800

    后台审核

--- admin/app/Http/Controllers/AdminController.php
+++ admin/app/Http/Controllers/AdminController.php
@@ -17,25 +17,59 @@ class AdminController extends BaseController
 		return view("admin.admin");
 	}
 
-
+	/**
+	 * [users_show 用户查看]
+	 * @return [type] [description]
+	 */
 	public function users_show()
 	{
-		$arr['users'] = DB::table('users')->rightJoin('login', 'users.loginid', '=', 'login.id')->get();
+		$arr['users'] = DB::table('users')->where("type",'=','1')->rightJoin('login', 'users.loginid', '=', 'login.id')->get();
 		//print_r($arr);die;
 		return view("admin.users_show",$arr);
 	}
 
+	/**
+	 * [internet_bar 网吧查看]
+	 * @return [type] [description]
+	 */
 	public function internet_bar()
 	{
-		$arr['users'] = DB::table('internet_bar')->leftjoin('login', 'internet_bar.loginid', '=', 'login.id')->get();
+		$arr['users'] = DB::table('internet_bar')->where("type",'=','2')->rightjoin('login', 'internet_bar.loginid', '=', 'login.id')->get();
 		//print_r($arr);die;
 		return view("admin.internet_bar",$arr);
 	}
 
+
+	/**
+	 * [invoice 发票查看]
+	 * @return [type] [description]
+	 */
 	public function invoice()
 	{
 		$arr['invoice'] = DB::table('invoice')->select()->get();
 		//print_r($arr);die;
 		return view("admin.invoice",$arr);
+	}
+
+	/**
+	 * [yes 网吧审核成功]
+	 * @return [type] [description]
+	 */
+	public function yes()
+	{
+		$id = $_GET['id'];
+		$res = DB::table('internet_bar')->where("id",$id)->update(['status'=>'1']);
+		return redirect("internet_bar");
+	}
+
+	/**
+	 * [no 网吧审核失败]
+	 * @return [type] [description]
+	 */
+	public function no()
+	{
+		$id = $_GET['id'];
+		$res = DB::table('internet_bar')->where("id",$id)->update(['status'=>'-1']);
+		return redirect("internet_bar");
 	}
 }
