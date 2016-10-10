c5e0950d75815dfffc59f08b0e1bb060dfb76f3e
hair <944259931@qq.com>
2016-10-10 21:15:14 +0800

    广告后台

--- app/app/Http/routes.php
+++ app/app/Http/routes.php
@@ -78,6 +78,7 @@ Route::group(['middleware' => ['webs']], function () {
     Route::post("uplodes_do",'MerchantController@uplodes_do');
     Route::any("addad",'MerchantController@addad');
     Route::any("addinto",'MerchantController@addinto');
+     Route::any("addlist",'MerchantController@addlist');
 
     //个人中心
     Route::get('user/index','UsersController@index');
